6b4da7328497c23d7e550d15a3cdade703cbf6bc
snowyyu <731444920@qq.com>
2017-06-29 18:16:53 +0800

    add HEROES array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,29 @@ export class Hero {
 	name: string;
 }
 
-
+const HEROES: Hero[] = [
+	{ id: 11, name: 'Mr. Nice' },
+  	{ id: 12, name: 'Narco' },
+  	{ id: 13, name: 'Bombasto' },
+  	{ id: 14, name: 'Celeritas' },
+  	{ id: 15, name: 'Magneta' },
+  	{ id: 16, name: 'RubberMan' },
+  	{ id: 17, name: 'Dynama' },
+  	{ id: 18, name: 'Dr IQ' },
+  	{ id: 19, name: 'Magma' },
+  	{ id: 20, name: 'Tornado' }
+	];
 
 @Component({
   selector: 'my-app',
-  template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>
+  template: `<h1>{{title}}</h1>
+			<ul class="heroes">
+  			<li *ngFor="let hero of heroes">
+   			<span class="badge">{{hero.id}}</span> {{hero.name}}
+  			</li>
+			</ul>
+
+  			<h2>{{hero.name}} details!</h2>
 			<div><label>id：</label>{{hero.id}}</div>
 			<div><label>name：</label>{{hero.name}}</div>
 			<div>
@@ -18,6 +36,9 @@ export class Hero {
 			</div>
   			`,
 })
+
+
+
 export class AppComponent  { 
 	
 	title = 'Tour of Heroes';
@@ -25,4 +46,10 @@ export class AppComponent  {
 		id: 1,
 		name: 'windstorm'
 		};
+
+
+
+	heroes = HEROES;
+
+
 	}

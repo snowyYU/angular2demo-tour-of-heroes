3d3849b5808bbf78b7ccd5103ddf3a1b48aa8489
snowyyu <731444920@qq.com>
2017-07-10 19:21:37 +0800

    sec5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,31 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} 	 from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent }  from './app.component';
-import { HeroDetailComponent } from './hero-detail.component'
+import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+
 
 @NgModule({
-  imports:      [ BrowserModule,FormsModule ],
-  declarations: [ AppComponent,HeroDetailComponent ],
-  bootstrap:    [ AppComponent ]
+    imports: [
+        BrowserModule, 
+        FormsModule,
+        RouterModule.forRoot([
+		{
+			path:'heroes',
+			component: HeroesComponent
+		}
+
+		])
+    ],
+    declarations: [AppComponent, HeroDetailComponent, HeroesComponent],
+    providers: [HeroService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+
+}
